f17b8ab22bab9392abd6a43f21a432ac22b517ec
mohamed saad <mohamed.saad_a136@fci.kfs.edu.eg>
2023-03-07 20:39:01 +0200

    Finishing Project

--- ClinicManagmentSystem.API/Program.cs
+++ ClinicManagmentSystem.API/Program.cs
@@ -34,7 +34,3 @@ app.MapHangfireDashboard();
 app.MapControllers();
 
 app.Run();
-
-
-// 6ab2b120-30a4-4258-a2bc-08db1a5cf055 | 3fa85f64-5717-4562-b3fc-2c963f66afa6 => clinics
-// 812399fb-e9e7-4834-f9f3-08db1caf1fac | c2eba0b6-1460-4985-121e-08db1cb39da6 => phys
