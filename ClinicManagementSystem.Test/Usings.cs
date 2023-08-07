8ac2776713d16ce372e8a4b2b10dbadb56a6c1d0
Muha Saad <mu7ammadsa3dd@gmail.com>
2023-08-07 14:18:40 +0300

    add AuthenticationServiceTests

--- ClinicManagementSystem.Test/Usings.cs
+++ ClinicManagementSystem.Test/Usings.cs
@@ -1,1 +1,6 @@
 global using Xunit;
+global using Moq;
+global using ClinicManagmentSystem.API.Dtos;
+global using ClinicManagmentSystem.API.Business.Services;
+global using ClinicManagmentSystem.API.Business.IServices;
+global using ClinicManagmentSystem.API.Controllers;
